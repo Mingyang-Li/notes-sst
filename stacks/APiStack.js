f70353d8572774f3053dd407a24830ca24d93369
Mingyang-Li <mingyangli118@gmail.com>
2023-08-13 20:56:43 +1200

    feat: add stripe handler for payments

--- stacks/APiStack.js
+++ stacks/APiStack.js
@@ -9,6 +9,9 @@ export function ApiStack({ stack, app }) {
         defaults: {
             function: {
                 bind: [table],
+                environment: {
+                    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+                },
             },
         },
         routes: {
@@ -16,6 +19,7 @@ export function ApiStack({ stack, app }) {
             "GET /notes/{id}": "packages/functions/src/get.main",
             "PUT /notes/{id}": "packages/functions/src/update.main",
             "DELETE /notes/{id}": "packages/functions/src/delete.main",
+            "POST /billing": "packages/functions/src/billing.main",
         },
     });
 
